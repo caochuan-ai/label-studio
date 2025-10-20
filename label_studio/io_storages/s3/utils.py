025096077e0cb801a14214f3c1ba49550d519756
wuqh97 <904531529@qq.com>
2025-10-20 18:07:34 +0800

    fixed huawei s3 adapt

--- label_studio/io_storages/s3/utils.py
+++ label_studio/io_storages/s3/utils.py
@@ -35,7 +35,7 @@ def get_client_and_resource(
     s3_endpoint = s3_endpoint or get_env('S3_ENDPOINT')
     if s3_endpoint:
         settings['endpoint_url'] = s3_endpoint
-    if "myhuaweicloud.com" in s3_endpoint:
+    if s3_endpoint is not None and "myhuaweicloud.com" in s3_endpoint:
         config = boto3.session.Config(s3={'addressing_style': 'virtual'})
     else:
         config = boto3.session.Config(signature_version='s3v4')
